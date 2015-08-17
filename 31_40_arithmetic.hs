7757a311d51f12e9cb4d999e0262012d31dc5948
Tim Steinbach <tsteinbach@trustwave.com>
2015-08-17 09:54:08 -0400

    34/35

--- 31_40_arithmetic.hs
+++ 31_40_arithmetic.hs
@@ -13,3 +13,16 @@ euclid_gcd n m
 -- Problem 33
 coprime :: Int -> Int -> Bool
 coprime n m = 1 == (euclid_gcd n m)
+
+-- Problem 34
+totient :: Int -> Int
+totient n = length (filter (==1) (map (\x -> euclid_gcd n x) [1..n-1]))
+
+-- Problem 35
+primeFactors :: Int -> [Int]
+primeFactors n = primeF n 2
+  where
+    primeF 1 _ = []
+    primeF n f
+      | (mod n f) == 0 = f : primeF (div n f) f
+      | otherwise = primeF n (f + 1)
